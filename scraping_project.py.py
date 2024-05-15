7a52c1e74b7e25161a9837f393bb1d9b9cf02ceb
zana-abdollahpour <zana.ap4@gmail.com>
2024-05-15 11:45:14 +0330

    add guess functionality

--- scraping_project.py.py
+++ scraping_project.py.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
+from random import choice
 
 all_quotes = []
 base_url = "https://quotes.toscrape.com"
@@ -21,3 +22,35 @@ while url:
     next_btn = soup.find(class_="next")
     url = next_btn.find("a")["href"] if next_btn else None
     sleep(2)
+
+
+quote = choice(all_quotes)
+remaining_guesses = 4
+print("Here's a quote: ")
+print(quote["text"])
+guess = ""
+
+while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
+    guess = input(f"Who said this quote? Guesses remaining: {
+                  remaining_guesses}\n")
+    if guess.lower() == quote["author"].lower():
+        print("GREAT! you guessed right!")
+        break
+
+    remaining_guesses -= 1
+    if remaining_guesses == 3:
+        res = requests.get(f"{base_url}{quote['bio_link']}")
+        soup = BeautifulSoup(res.text, "html.parser")
+        birth_date = soup.find(class_="author-born-date").get_text()
+        birth_place = soup.find(class_="author-born-location").get_text()
+        print(f"Here's a hint: The author was born in {
+              birth_place} on {birth_date}")
+    elif remaining_guesses == 2:
+        print(f"Here's a hint: The author's first name starts with:  {
+            quote['author'][0]} ")
+    elif remaining_guesses == 1:
+        print(f"Here's a hint: The author's last name starts with:  {
+            quote['author'].split(" ")[1][0]} ")
+    else:
+        print("Sorry, you ran out of guesses :(")
+        print(f"the answer was {quote['author']}")
