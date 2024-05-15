0df5a46776bcdf9bfb1896509f5dfc904e045001
zana-abdollahpour <zana.ap4@gmail.com>
2024-05-15 16:42:19 +0330

    add crawler via scrapy

---
+++ book_scraper.py
@@ -0,0 +1,17 @@
+import scrapy
+
+
+class BookSpider(scrapy.Spider):
+    name = "bookspider"
+    start_urls = ["http://books.toscrape.com"]
+
+    def parse(self, response):
+        for article in response.css("article.product_pod"):
+            yield {
+                "price": article.css(".price_color::text").extract_first(),
+                "title": article.css("h3 > a::attr(title)").extract_first()
+            }
+            next = response.css(
+                ".next > a::attr(href)").extract_first()  # next page
+            if next:
+                yield response.follow(next, self.parse)
