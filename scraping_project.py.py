66ec989648d4cbaf675bace9fcd6ba6048121e84
zana-abdollahpour <zana.ap4@gmail.com>
2024-05-15 13:05:58 +0330

    refactor logic to functions

--- scraping_project.py.py
+++ scraping_project.py.py
@@ -3,54 +3,73 @@ from bs4 import BeautifulSoup
 from time import sleep
 from random import choice
 
-all_quotes = []
-base_url = "https://quotes.toscrape.com"
+BASE_URL = "https://quotes.toscrape.com"
 url = "/page/1"
 
-while url:
-    res = requests.get(f"{base_url}{url}")
-    soup = BeautifulSoup(res.text)
-    quotes = soup.find_all(class_="quote")
 
-    for quote in quotes:
-        all_quotes.append({
-            "text": quote.find(class_="text").get_text(),
-            "author": quote.find(class_="author").get_text(),
-            "bio_link": quote.find("a")["href"]
-        })
+def scrape_quotes():
+    all_quotes = []
+    while url:
+        res = requests.get(f"{BASE_URL}{url}")
+        soup = BeautifulSoup(res.text)
+        quotes = soup.find_all(class_="quote")
 
-    next_btn = soup.find(class_="next")
-    url = next_btn.find("a")["href"] if next_btn else None
-    sleep(2)
+        for quote in quotes:
+            all_quotes.append({
+                "text": quote.find(class_="text").get_text(),
+                "author": quote.find(class_="author").get_text(),
+                "bio_link": quote.find("a")["href"]
+            })
+
+        next_btn = soup.find(class_="next")
+        url = next_btn.find("a")["href"] if next_btn else None
+        sleep(2)
+    return all_quotes
 
 
-quote = choice(all_quotes)
-remaining_guesses = 4
-print("Here's a quote: ")
-print(quote["text"])
-guess = ""
+def start_game(quotes):
+    quote = choice(quotes)
+    remaining_guesses = 4
+    print("Here's a quote: ")
+    print(quote["text"])
+    guess = ""
 
-while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
-    guess = input(f"Who said this quote? Guesses remaining: {
-                  remaining_guesses}\n")
-    if guess.lower() == quote["author"].lower():
-        print("GREAT! you guessed right!")
-        break
+    while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
+        guess = input(f"Who said this quote? Guesses remaining: {
+            remaining_guesses}\n")
+        if guess.lower() == quote["author"].lower():
+            print("GREAT! you guessed right!")
+            break
 
-    remaining_guesses -= 1
-    if remaining_guesses == 3:
-        res = requests.get(f"{base_url}{quote['bio_link']}")
-        soup = BeautifulSoup(res.text, "html.parser")
-        birth_date = soup.find(class_="author-born-date").get_text()
-        birth_place = soup.find(class_="author-born-location").get_text()
-        print(f"Here's a hint: The author was born in {
-              birth_place} on {birth_date}")
-    elif remaining_guesses == 2:
-        print(f"Here's a hint: The author's first name starts with:  {
-            quote['author'][0]} ")
-    elif remaining_guesses == 1:
-        print(f"Here's a hint: The author's last name starts with:  {
-            quote['author'].split(" ")[1][0]} ")
+        remaining_guesses -= 1
+        if remaining_guesses == 3:
+            res = requests.get(f"{BASE_URL}{quote['bio_link']}")
+            soup = BeautifulSoup(res.text, "html.parser")
+            birth_date = soup.find(class_="author-born-date").get_text()
+            birth_place = soup.find(
+                class_="author-born-location").get_text()
+            print(f"Here's a hint: The author was born in {
+                birth_place} on {birth_date}")
+        elif remaining_guesses == 2:
+            print(f"Here's a hint: The author's first name starts with:  {
+                quote['author'][0]} ")
+        elif remaining_guesses == 1:
+            print(f"Here's a hint: The author's last name starts with:  {
+                quote['author'].split(" ")[1][0]} ")
+        else:
+            print("Sorry, you ran out of guesses :(")
+            print(f"the answer was {quote['author']}")
+
+    again = input("Would you like to play again (y/n)? ")
+    while again.lower() not in ("y", "n"):
+        again = input("Would you like to play again (y/n)? ")
+
+    if again.lower() == "y":
+        print("OK, You play again...")
+        return start_game(quotes)
     else:
-        print("Sorry, you ran out of guesses :(")
-        print(f"the answer was {quote['author']}")
+        print("OK, Goodbye!")
+
+
+quotes = scrape_quotes()
+start_game(quotes)
